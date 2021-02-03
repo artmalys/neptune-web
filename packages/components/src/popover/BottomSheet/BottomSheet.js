45400382a0e136238d53e7e0de537f9d64aadccb
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-02-03 20:57:25 +0000

    chore: initial setup for BottomSheet

--- packages/components/src/popover/BottomSheet/BottomSheet.js
+++ packages/components/src/popover/BottomSheet/BottomSheet.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import Types from 'prop-types';
 import Dimmer from '../../dimmer';
 import SlidingPanel from '../../slidingPanel';
+import './BottomSheet.css';
 
-const BottomSheet = ({ children, open }) => (
-  <Dimmer open={open}>
-    <SlidingPanel open={open} position="bottom">
-      {children}
-    </SlidingPanel>
-  </Dimmer>
-);
+const BottomSheet = forwardRef((props, ref) => {
+  const { children, open } = props;
+
+  return (
+    <Dimmer open={open}>
+      <SlidingPanel open={open} position="bottom" transparent>
+        <div ref={ref} className="np-bottom-sheet p-x-2 p-t-4 p-b-2">
+          {children}
+        </div>
+      </SlidingPanel>
+    </Dimmer>
+  );
+});
 
 BottomSheet.propTypes = {
   children: Types.node.isRequired,
