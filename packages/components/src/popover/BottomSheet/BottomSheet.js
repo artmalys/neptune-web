95ec223f397766440852a775d3afaa84471eb5b4
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-02-03 15:40:38 +0000

    chore: update bottomSheet

--- packages/components/src/popover/BottomSheet/BottomSheet.js
+++ packages/components/src/popover/BottomSheet/BottomSheet.js
@@ -3,15 +3,13 @@ import Types from 'prop-types';
 import Dimmer from '../../dimmer';
 import SlidingPanel from '../../slidingPanel';
 
-const BottomSheet = ({ children, open }) => {
-  return (
-    <Dimmer open={open}>
-      <SlidingPanel open={open} position="bottom">
-        <div className="p-y-2 p-x-3">{children}</div>
-      </SlidingPanel>
-    </Dimmer>
-  );
-};
+const BottomSheet = ({ children, open }) => (
+  <Dimmer open={open}>
+    <SlidingPanel open={open} position="bottom">
+      {children}
+    </SlidingPanel>
+  </Dimmer>
+);
 
 BottomSheet.propTypes = {
   children: Types.node.isRequired,
